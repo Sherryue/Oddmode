cec48ece32ffc4166f0103e53b781c851e136399
Sherryue <32973744+Sherryue@users.noreply.github.com>
2017-10-25 09:44:20 -0700

    varied distance odd mode

--- Bridge/Extracting_Q.m
+++ Bridge/Extracting_Q.m
@@ -11,9 +11,9 @@ clear; clc;
 % % S22 = zeros(Leng, Num);
 % gap = str2num(Names(:,20:21));  %need to be changed with different file names
 
-gap = [256].';
+gap = [50,124,150:100:350].';
 Num = length(gap);
-Pref = 'Xodd_D';
+Pref = 'Xodd_W8_D';
 Sufx = num2str(gap);
 Sufx(1,1) = '0';
 Names = [repmat(Pref,Num,1),Sufx];
@@ -31,15 +31,23 @@ for ki = 1:Num
 %     S22(:,ki) = (permute(S.Parameters(2,2,:),[3,2,1]));
     xdata = abs(S.Frequencies)./1e9;
     ydata1 = abs(permute(S.Parameters(2,1,:),[3,2,1]));
+    t_est = ydata1(1);
 %     xdata = xdata(200:800);
 %     ydata1 = ydata1(200:800);
     
     [~,ind_min] = min(ydata1);
     f_min = xdata(ind_min);
     
-    fun1 = @(x, xdata) (x(5).*(x(1).^2+4.*((xdata-x(2))-x(4)).^2)./((x(1)+x(3)).^2+4.*((xdata-x(2))).^2)).^(1);
-    %x(1) kappa_i/2pi, x(2) f_0, x(3) kappa_e/2pi, x(4) Fano parameter, x(5) global offset;
-    x0 = [4e-5, f_min, 1e-4, 0, 1];    %need to be changed! weakercp[0, 7.972432, 2e-6]; gap200[0, 7.9606892, 3e-7]
+%     fun1 = @(x, xdata) (x(5).*(x(1).^2+4.*((xdata-x(2))-x(4)).^2)./((x(1)+x(3)).^2+4.*((xdata-x(2))).^2)).^(1);
+%     %x(1) kappa_i/2pi, x(2) f_0, x(3) kappa_e/2pi, x(4) Fano parameter, x(5) global offset;
+%     x0 = [3.68e-5, f_min, 1.27e-4, 0, 1];    %need to be changed! weakercp[0, 7.972432, 2e-6]; gap200[0, 7.9606892, 3e-7]
+    
+    fun1 = @(x, xdata) ((x(4).*(x(1)+x(3))-2.*x(5).*(xdata-x(2))-x(3)).^2+(2.*x(4).*(xdata-x(2))+x(5).*(x(1)+x(3))).^2)./...
+        ((x(1)+x(3)).^2+4.*(xdata-x(2)).^2);
+    % x(1) kappa_i/2pi, x(2) f_0, x(3) kappa_e/2pi, x(4) Real(t), x(5)
+    % Imag(t)
+    x0 = [3.68e-5, f_min, 1.27e-4, real(t_est), imag(t_est)]; 
+    
     [x1,resnorm,residual,exitflag,output,lambda,J] = lsqcurvefit(fun1, x0, xdata, ydata1.^2);
     
     fit_para(ki,:) = abs(x1);
@@ -50,17 +58,19 @@ for ki = 1:Num
     Ci_para(ki,3:4) = ci(2,:);
     Ci_para(ki,5:6) = ci(3,:);
     
-    MAGS21_fit = sqrt(x1(5).*(x1(1).^2+4.*((xdata-x1(2))-x1(4)).^2)./((x1(1)+x1(3)).^2+4.*((xdata-x1(2))).^2));
-    figure; %plot(xdata, 10.*log10(permute(S.Parameters(1,1,:),[3,2,1])))
-    plot(xdata, 10.*log10(MAGS21_fit));
-    hold on; plot(xdata, 10.*log10(ydata1), '--');
-    xlabel('Frequency (GHz)')
-    ylabel('Mag(S21) (dB)')
+%     MAGS21_fit = sqrt(x1(5).*(x1(1).^2+4.*((xdata-x1(2))-x1(4)).^2)./((x1(1)+x1(3)).^2+4.*((xdata-x1(2))).^2));
+    MAGS21_fit = sqrt(((x1(4).*(x1(1)+x1(3))-2.*x1(5).*(xdata-x1(2))-x1(3)).^2+(2.*x1(4).*(xdata-x1(2))+x1(5).*(x1(1)+x1(3))).^2)./...
+        ((x1(1)+x1(3)).^2+4.*(xdata-x1(2)).^2));
+%     figure; %plot(xdata, 10.*log10(permute(S.Parameters(1,1,:),[3,2,1])))
+%     plot(xdata, 10.*log10(MAGS21_fit));
+%     hold on; plot(xdata, 10.*log10(ydata1), '--');
+%     xlabel('Frequency (GHz)')
+%     ylabel('Mag(S21) (dB)')
 end
 
-% figure; errorbar(gap, fit_para(:,2), fit_para(:,2)-Ci_para(:,3),fit_para(:,2)-Ci_para(:,4),'d-');
-% figure; errorbar(gap, fit_para(:,1), fit_para(:,1)-Ci_para(:,1),fit_para(:,1)-Ci_para(:,2),'d-');
-% figure; errorbar(gap, fit_para(:,3), fit_para(:,3)-Ci_para(:,5),fit_para(:,3)-Ci_para(:,6),'d-');
+figure; errorbar(gap, fit_para(:,2), fit_para(:,2)-Ci_para(:,3),fit_para(:,2)-Ci_para(:,4),'d-');
+figure; errorbar(gap, fit_para(:,1), fit_para(:,1)-Ci_para(:,1),fit_para(:,1)-Ci_para(:,2),'d-');
+figure; errorbar(gap, fit_para(:,3), fit_para(:,3)-Ci_para(:,5),fit_para(:,3)-Ci_para(:,6),'d-');
 
 % figure (1);hold on; errorbar(gap, fit_para(:,2), fit_para(:,2)-Ci_para(:,3),fit_para(:,2)-Ci_para(:,4),'d-');
 % figure(2);hold on; errorbar(gap, fit_para(:,1), fit_para(:,1)-Ci_para(:,1),fit_para(:,1)-Ci_para(:,2),'d-');
